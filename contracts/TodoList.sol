f3d5411486b2b58266f72d80c0809d8bac5b5903
zlatax <64391879+zlatax@users.noreply.github.com>
2021-09-20 19:50:47 -0400

    updating js file for front-end logic

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,27 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
+	//state varaibles
 	uint public taskCount = 0;
+
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	mapping(uint => Task) public tasks;
+
+
+	// similar to __init__ to add default functions when instantiated
+	constructor() public {
+		createTask("watch lectures!");
+	}
+
+	function createTask(string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = Task(taskCount,_content, false);
+	}
+
 }
 
